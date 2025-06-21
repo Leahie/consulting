393b58d2d3cfb71027596f83bb35a215c97526b0
Leah Zhang <LeahZhang1595995@gmail.com>
2025-06-21 11:57:53 -0400

    fix the text and footer

--- src/(components)/Footer.tsx
+++ src/(components)/Footer.tsx
@@ -3,8 +3,8 @@ import FooterInfo from "@/(assets)/Footer.json"
 import FooterInfo1 from "@/(assets)/Footer1.json"
 export default function Footer(){
     return(
-        <div className="flex mt-[2em] text-slate-600">
-            <div className=" bg-black-color flex  flex-wrap justify-start pt-[2.5em] pb-[1.3em] px-[2em]  gap-[2em] w-[50%]">
+        <div className="flex mt-[2em] text-slate-600 flex-col sm:flex-row">
+            <div className=" bg-black-color flex  flex-wrap sm:justify-start  sm:pt-[2.5em] pb-[1.3em] px-[2em]  gap-[2em] ">
                 {/* <div><FaSpotify />
                     <div>
                         <h1>On Repeat</h1>
@@ -14,8 +14,8 @@ export default function Footer(){
                 </div> */}
                 <p >&copy; 2025  Leah Zhang</p>
             </div>
-            <div className="w-[50%] bg-black-color flex flex-wrap justify-end pt-[2.5em] pb-[1.3em] px-[2em]  gap-[2em]">
-                <div className=" flex gap-5">
+            <div className="w-[50%] bg-black-color flex flex-wrap sm:justify-end sm:pt-[2.5em] pb-[1.3em] px-[2em]  gap-[2em]">
+                <div className=" flex gap-5 sm:flex-row flex-col ">
                     {
                     FooterInfo1.map(({name, link}, index)=>(
                             <a className=" font-normal" href={link} key={index}>{name} </a>
